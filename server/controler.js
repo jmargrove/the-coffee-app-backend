ce9fcc84932e1889a1757f5cfba6152ee19c0c07
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2017-12-10 20:49:14 +0100

    feat: coffee app backend basic done

--- server/controler.js
+++ server/controler.js
@@ -1,28 +1,20 @@
-const mongodb = require('mongodb')
 const R = require('r-script')
 const fs = require('fs')
-//
-exports.postReq = async (ctx, next) => {
-  const out = await R("./Rcode/Rrainfall.R")
-    .data(ctx.request.body)
-    .callSync()
-      console.log("Data returned from R:", out)
-  ctx.response.body = await out
-}
+
 
 exports.modelData = async (ctx, next) => {
   console.log(ctx.request.body)
-  const out = await R("./Rcode/fullerCoffeeModel/RcoffeeModelNew.R")
+  const out = await R("./../../the-coffee-app-Model/fullerCoffeeModel/RcoffeeModelNew2.R")
     .data(ctx.request.body)
     .callSync()
       console.log("Data returned from R:", out)
-  ctx.response.body = await out
+  ctx.response.body = await out;
 }
 
 
 exports.modelOptimized = async (ctx, next) => {
   console.log(ctx.request.body)
-  const out = await R("./Rcode/fullerCoffeeModel/RcoffeeModelOptimize.R")
+  const out = await R("./../../the-coffee-app-Model/fullerCoffeeModel/RcoffeeModelOptimize2.R")
     .data(ctx.request.body)
     .callSync()
       console.log("Data returned from R:", out)
--- server/router.js
+++ server/router.js
@@ -1,13 +1,9 @@
 const Router = require('koa-router')
 const router = new Router()
-
-const Koa = require('koa');
-const app = new Koa();
 const controler = require('./controler.js')
 
-router.post('/Rcode', controler.postReq)
 router.post('/model-data', controler.modelData)
 router.post('/optimize', controler.modelOptimized)
-router.get('/Rcode')
+
 
 module.exports = router
